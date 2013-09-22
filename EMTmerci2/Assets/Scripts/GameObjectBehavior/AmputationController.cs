9ef7eb1cd271067860881b7f200a70798d44bfcc
U-Akragas\agrub_000 <agrub_000@Akragas.(none)>
2013-09-22 05:42:26 -0400

    BULLSHIT arm cutting is kinda working.

--- EMTmerci2/Assets/Scripts/GameObjectBehavior/AmputationController.cs
+++ EMTmerci2/Assets/Scripts/GameObjectBehavior/AmputationController.cs
@@ -17,12 +17,12 @@ public class AmputationController : TouchObject {
 
     public override void OnPull(GestureObject g)
     {
-        print(g.StartPosition + " " + g.EndPosition);
-        Vector3 tempStart = new Vector3(g.StartPosition.x, g.StartPosition.y, Camera.main.nearClipPlane + 5.0f);
-        Vector3 tempEnd = new Vector3(g.StartPosition.x, g.StartPosition.y, Camera.main.nearClipPlane);
+        Vector3 tempStart = new Vector3(g.StartPosition.x, g.StartPosition.y, Camera.main.nearClipPlane);
+        Vector3 tempEnd = new Vector3(g.EndPosition.x, g.EndPosition.y, Camera.main.nearClipPlane);
         Vector3 worldStart = Camera.main.ScreenToWorldPoint(tempStart);
         Vector3 worldEnd = Camera.main.ScreenToWorldPoint(tempEnd);
-        print(worldStart + " " + worldEnd);
+        print("START " + worldStart.x + ", " + worldStart.y + ", " + worldStart.z);
+        print("END " + worldEnd.x + ", " + worldEnd.y + ", " + worldEnd.z);
         GetComponent<Sliceable>().SliceMesh(worldStart, worldEnd);
     }
 }
--- EMTmerci2/Assets/Scripts/GameObjectBehavior/Sliceable.cs
+++ EMTmerci2/Assets/Scripts/GameObjectBehavior/Sliceable.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Sliceable : MonoBehaviour {
 
@@ -19,42 +20,61 @@ public class Sliceable : MonoBehaviour {
 
     public void SliceMesh(Vector3 sliceStart, Vector3 sliceEnd)
     {
-        Vector3 slicePlusZ = new Vector3(sliceEnd.x, sliceEnd.y, sliceEnd.z + 1000);
-        Mesh initialMesh = GetComponent<MeshFilter>().mesh;
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
 
-        Vector3[] vertices = initialMesh.vertices;
-
-        Transform clone = (Transform)Instantiate(transform, transform.position + (new Vector3(.5f, 0, 0)), Quaternion.identity);
+        Transform clone = (Transform)Instantiate(transform, transform.position, Quaternion.identity);
+        transform.position -= transform.InverseTransformPoint(transform.TransformPoint(new Vector3(.1f, 0, 0)));
+        clone.rotation = transform.rotation;
         Mesh cloneMesh = clone.GetComponent<MeshFilter>().mesh;
         Vector3[] cloneVertices = cloneMesh.vertices;
 
+        Vector3 armSpaceStart = transform.InverseTransformPoint(sliceStart);
+        Vector3 armSpaceEnd = transform.InverseTransformPoint(sliceEnd);
 
+        print("ARM START " + armSpaceStart.x + ", " + armSpaceStart.y + ", " + armSpaceStart.z);
+        print("ARM END " + armSpaceEnd.x + ", " + armSpaceEnd.y + ", " + armSpaceEnd.z);
 
-        Plane slicePlane = new Plane(sliceStart, sliceEnd, slicePlusZ);
-        
-        
+        Vector3 p1 = transform.TransformPoint(new Vector3(armSpaceStart.x, transform.collider.bounds.extents.y, transform.collider.bounds.extents.z));
+        Vector3 p2 = transform.TransformPoint(new Vector3(armSpaceEnd.x, -transform.collider.bounds.extents.y, transform.collider.bounds.extents.z));
+        Vector3 p3 = transform.TransformPoint(new Vector3(armSpaceEnd.x, -transform.collider.bounds.extents.y, -transform.collider.bounds.extents.z));
+
+        print("P1 " + p1);
+        print("P2 " + p2);
+        print("P3 " + p3);
+
+        Plane slicePlane = new Plane(p1, p2, p3);
+
+        Vector3 planeCenter = new Vector3((p1.x + p3.x) / 2, (p1.y + p3.y) / 2, (p1.z + p3.z) / 2);
+        print("CENTER OF PLANE " + planeCenter);
 
         for (int i = 0; i < vertices.Length; i++)
         {
-            Vector3 vertex = transform.TransformPoint(vertices[i]);
-            Vector3 cloneVertex = transform.TransformPoint(cloneVertices[i]);
+            Vector3 vertex = vertices[i];
+            Vector3 cloneVertex = cloneVertices[i];
 
             if (slicePlane.GetSide(vertex))
             {
-                vertices[i] = transform.InverseTransformPoint(new Vector3(vertex.x, vertex.y - (slicePlane.GetDistanceToPoint(vertex)), vertex.z));
-                cloneVertices[i] = transform.InverseTransformPoint(new Vector3(vertex.x, vertex.y, vertex.z));
+                vertices[i] = new Vector3(vertex.x - (slicePlane.GetDistanceToPoint(vertex)), 0, 0);
             }
             else
             {
-                cloneVertices[i] = transform.InverseTransformPoint(new Vector3(vertex.x, vertex.y - (slicePlane.GetDistanceToPoint(vertex)), vertex.z));
+                cloneVertices[i] = new Vector3(vertex.x - (slicePlane.GetDistanceToPoint(vertex)), 0, 0);
             }
         }
 
-        initialMesh.vertices = vertices;
-        initialMesh.RecalculateBounds();
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
 
         cloneMesh.vertices = cloneVertices;
         cloneMesh.RecalculateBounds();
-        
+
+        BoxCollider bc = GetComponent<BoxCollider>();
+        bc.size = new Vector3(mesh.bounds.size.x * 0.8f, mesh.bounds.size.y * 0.8f, mesh.bounds.size.z * 0.8f); // reset size, bit smaller than real
+        bc.center = mesh.bounds.center;
+
+        BoxCollider cloneBc = clone.GetComponent<BoxCollider>();
+        cloneBc.size = new Vector3(cloneMesh.bounds.size.x * 0.8f, cloneMesh.bounds.size.y * 0.8f, cloneMesh.bounds.size.z * 0.8f);
+        cloneBc.center = cloneMesh.bounds.center;
     }
 }
