1701cc80d8ed5970ac047a7cb327c2b4530a9b46
Paul Tschirgi <pault1@umbc.edu>
2013-09-22 02:07:28 -0400

    Me working with textured Merci

---
+++ EMTmerci2/Assets/Scripts/holdhands.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class holdhands : MonoBehaviour {
+	
+public bool WristSnap = false;
+public Transform WristHeld;
+public Transform Parent;
+
+	// Use this for initialization
+	void WristPosParent () {
+	
+		WristSnap = true;
+		transform.parent= null;
+		transform.parent = WristHeld;
+	}
+	
+	void ResetParent () {
+	
+//		transform.parent = Bones;
+		
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(WristSnap ==true ){
+//			transform.position = WristHeld.position;
+		}
+		
+	}
+}
